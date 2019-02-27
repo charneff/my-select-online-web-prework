dba1fe2098d4df698ec339c25149470bdc72b7ea
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-27 19:20:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,8 @@
-collection = []
 def my_select(collection)
  i = 0
    while i < collection.length
+     yield(collection[i]) == true
      i += 1
-     yield(collection) == true
    end
    
  collection
